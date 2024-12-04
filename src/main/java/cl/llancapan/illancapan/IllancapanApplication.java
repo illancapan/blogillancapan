c4433ca5b1d3777f538386bf63ca945ba48150bc
illancapan <igorllancapan@gmail.com>
2024-12-04 16:15:39 -0300

    first commit

---
+++ src/main/java/cl/llancapan/illancapan/IllancapanApplication.java
@@ -0,0 +1,13 @@
+package cl.llancapan.illancapan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllancapanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllancapanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/llancapan/illancapan/IllancapanApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.llancapan.illancapan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllancapanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
